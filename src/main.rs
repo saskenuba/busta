d99e2b1a052785ad9f130daa1a8b2519ffabd986
Martin <martin@hotmail.com.br>
2018-12-24 02:51:51 -0200

    trying to get vec class as a module

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::fmt;
+mod vec;
+
+#[derive(Debug)]
+struct RGBPixel {
+    r: i32,
+    g: i32,
+    b: i32,
+}
+
+impl std::fmt::Display for RGBPixel {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{} {} {}", self.r, self.g, self.b)
+    }
+}
+
+
+
+
+
+fn outputImage(width: i32, height: i32) {
+    for i in 0..width {
+        for j in 0..height {
+            let basinga = i as f64 / width as f64;
+            let hehe = j as f64 / height as f64;
+            let wat = 0.2;
+
+            let okok = RGBPixel {
+                r: (255.99 * basinga) as i32,
+                g: (255.99 * hehe) as i32,
+                b: (255.99 * wat) as i32,
+            };
+
+            println!("{:?}", okok);
+        }
+    }
+}
+
+fn main() {
+    outputImage(5, 5);
+}
---
+++ src/vec.rs
@@ -0,0 +1,12 @@
+use std::ops;
+
+mod vec3 {
+
+    pub mod struct Vec3 {
+        vec3: Vec<i32>,
+    }
+
+    pub impl ops::Add for Vec3 {
+        unimplemented!();
+    }
+}
