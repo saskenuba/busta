60c86186b20b69df0da33f02bbbd3f4d4101c94e
Martin <martin@hotmail.com.br>
2018-12-29 20:51:14 -0200

    refactored operator overloading using macros

--- src/vec.rs
+++ src/vec.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::*;
 
 #[derive(Debug)]
 pub struct Vec3 {
@@ -12,11 +12,18 @@ impl Vec3 {
         }
     }
     pub fn dot(v1: Vec3, v2: Vec3) -> Vec3 {
+        let mut result: [f64; 3] = [0.0; 3];
+        for ((resultval, x), y) in result.iter_mut().zip(&v1.vector).zip(&v2.vector) {
+            *resultval = x * y;
+        }
+        Vec3 { vector: result }
+    }
+    pub fn cross(v1: Vec3, v2: Vec3) -> Vec3 {
         Vec3 {
             vector: [
-                v1.vector[0] * v2.vector[0],
-                v1.vector[1] * v2.vector[1],
-                v1.vector[2] * v2.vector[2],
+                v1.vector[1] * v2.vector[2] - v1.vector[2] * v2.vector[1],
+                -(v1.vector[0] * v2.vector[2] - v1.vector[2] * v2.vector[0]),
+                v1.vector[0] * v2.vector[1] - v1.vector[1] * v2.vector[0],
             ],
         }
     }
@@ -25,23 +32,51 @@ impl Vec3 {
 impl PartialEq for Vec3 {
     fn eq(&self, other: &Vec3) -> bool {
         // iter both, and for each element compare, im getting gud at this
-        self.vector.iter().zip(other.vector.iter()).all(|(a,b)| a == b)
+        self.vector
+            .iter()
+            .zip(other.vector.iter())
+            .all(|(a, b)| a == b)
     }
 }
 
-impl Add for Vec3 {
-    type Output = Vec3;
-
-    fn add(self, other: Vec3) -> Vec3 {
-        Vec3 {
-            vector: [
-                self.vector[0] + other.vector[0],
-                self.vector[1] + other.vector[1],
-                self.vector[2] + other.vector[2],
-            ],
+macro_rules! overload_vec3 {
+    ($oper:tt, $oper_name:ident, $oper_name_lowercase:ident, Vec3) => {
+        impl $oper_name<Vec3> for Vec3 {
+            type Output = Vec3;
+            fn $oper_name_lowercase(self, other: Vec3) -> Vec3 {
+                Vec3 {
+                    vector: [
+                        self.vector[0] $oper other.vector[0],
+                        self.vector[1] $oper other.vector[1],
+                        self.vector[2] $oper other.vector[2],
+                    ],
+                }
+            }
         }
-    }
+    };
+    ($oper:tt, $oper_name:ident, $oper_name_lowercase:ident, $custom_sig:ty) => {
+        impl $oper_name<$custom_sig> for Vec3 {
+            type Output = Vec3;
+            fn $oper_name_lowercase(self, other: $custom_sig) -> Vec3 {
+                Vec3 {
+                    vector: [
+                        self.vector[0] $oper other,
+                        self.vector[1] $oper other,
+                        self.vector[2] $oper other,
+                    ],
+                }
+            }
+        }
+    };
 }
+
+// Here we overload all binary operators
+overload_vec3!(+, Add, add, Vec3);
+overload_vec3!(-, Sub, sub, Vec3);
+overload_vec3!(*, Mul, mul, Vec3);
+overload_vec3!(/, Div, div, Vec3);
+overload_vec3!(*, Mul, mul, f64);
+overload_vec3!(/, Div, div, f64);
 
 #[test]
 fn dot() {
@@ -57,4 +92,42 @@ fn add() {
     let vector2 = Vec3::new(2.0, 3.0, 4.0);
 
     assert_eq!(vector1 + vector2, Vec3::new(3.0, 5.0, 7.0));
+}
+
+#[test]
+fn sub() {
+    let vector1 = Vec3::new(1.0, 2.0, 3.0);
+    let vector2 = Vec3::new(2.0, 3.0, 4.0);
+
+    assert_eq!(vector1 - vector2, Vec3::new(-1.0, -1.0, -1.0));
+}
+
+#[test]
+fn mul() {
+    let vector1 = Vec3::new(1.0, 2.0, 3.0);
+    let vector2 = Vec3::new(2.0, 3.0, 4.0);
+
+    assert_eq!(vector1 * vector2, Vec3::new(2.0, 6.0, 12.0));
+
+    let vector3 = Vec3::new(2.0, 20.0, -7.5);
+    assert_eq!(vector3 * 3.0, Vec3::new(6.0, 60.0, -22.5));
+}
+
+#[test]
+fn div() {
+    let vector1 = Vec3::new(5.0, 10.0, 2.5);
+    let vector2 = Vec3::new(10.0, 40.0, 5.0);
+
+    assert_eq!(vector2 / vector1, Vec3::new(2.0, 4.0, 2.0));
+
+    let vector3 = Vec3::new(2.0, 10.0, 3.4);
+    assert_eq!(vector3 / 2.0, Vec3::new(1.0, 5.0, 1.7));
+}
+
+#[test]
+fn cross() {
+    let vector1 = Vec3::new(1.0, 2.0, 3.0);
+    let vector2 = Vec3::new(2.0, 3.0, 4.0);
+
+    assert_eq!(Vec3::cross(vector1, vector2), Vec3::new(-1.0, 2.0, -1.0));
 }
